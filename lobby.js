f95a4e04405898c56677dd409f0e265d88e76895
chottomattenft <103018114+chottomattenft@users.noreply.github.com>
2022-04-06 00:36:47 +0200

    Update lobby.js

--- lobby.js
+++ lobby.js
@@ -1,278 +1,284 @@
-// contract address on Kovan:
-const rewardProgramAddress = '0x9724cC3Beff4316dfdeA0533c4ba5eC6C7E809e7'
+// contract address on Rinkeby:
+const rewardProgramAddress = '0x88B48F654c30e99bc2e4A1559b4Dcf1aD93FA656'
 
 // add contract ABI:
 const rewardProgramABI =
 
 [
-{
-  "inputs": [
-    {
-      "internalType": "address",
-      "name": "_nftAddress",
-      "type": "address"
-    },
-    {
-      "internalType": "uint256",
-      "name": "_nftID",
-      "type": "uint256"
-    }
-  ],
-  "stateMutability": "nonpayable",
-  "type": "constructor"
-},
-{
-  "anonymous": false,
-  "inputs": [
-    {
-      "indexed": false,
-      "internalType": "address",
-      "name": "redeemer",
-      "type": "address"
-    },
-    {
-      "indexed": false,
-      "internalType": "enum PerkManager.Perk",
-      "name": "perk",
-      "type": "uint8"
-    },
-    {
-      "indexed": false,
-      "internalType": "uint256",
-      "name": "date",
-      "type": "uint256"
-    }
-  ],
-  "name": "NewRedemption",
-  "type": "event"
-},
-{
-  "anonymous": false,
-  "inputs": [
-    {
-      "indexed": true,
-      "internalType": "address",
-      "name": "previousOwner",
-      "type": "address"
-    },
-    {
-      "indexed": true,
-      "internalType": "address",
-      "name": "newOwner",
-      "type": "address"
-    }
-  ],
-  "name": "OwnershipTransferred",
-  "type": "event"
-},
-{
-  "inputs": [
-    {
-      "internalType": "enum PerkManager.Perk",
-      "name": "_perk",
-      "type": "uint8"
-    }
-  ],
-  "name": "redeemPerk",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-},
-{
-  "inputs": [],
-  "name": "renounceOwnership",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-},
-{
-  "anonymous": false,
-  "inputs": [
-    {
-      "indexed": false,
-      "internalType": "address",
-      "name": "ownerOfNFT",
-      "type": "address"
-    },
-    {
-      "indexed": false,
-      "internalType": "uint256",
-      "name": "newSecurityID",
-      "type": "uint256"
-    },
-    {
-      "indexed": false,
-      "internalType": "uint256",
-      "name": "date",
-      "type": "uint256"
-    }
-  ],
-  "name": "securityIDchanged",
-  "type": "event"
-},
-{
-  "inputs": [
-    {
-      "internalType": "uint256",
-      "name": "_securityID",
-      "type": "uint256"
-    }
-  ],
-  "name": "setSecurityID",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-},
-{
-  "inputs": [
-    {
-      "internalType": "address",
-      "name": "newOwner",
-      "type": "address"
-    }
-  ],
-  "name": "transferOwnership",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-},
-{
-  "inputs": [],
-  "name": "verifyAddress",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-},
-{
-  "inputs": [],
-  "name": "yearlyUpdate",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-},
-{
-  "inputs": [
-    {
-      "internalType": "enum PerkManager.Perk",
-      "name": "",
-      "type": "uint8"
-    }
-  ],
-  "name": "amountPerk",
-  "outputs": [
-    {
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-},
-{
-  "inputs": [],
-  "name": "deadline",
-  "outputs": [
-    {
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-},
-{
-  "inputs": [],
-  "name": "isVerified",
-  "outputs": [
-    {
-      "internalType": "bool",
-      "name": "",
-      "type": "bool"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-},
-{
-  "inputs": [],
-  "name": "nftAddress",
-  "outputs": [
-    {
-      "internalType": "address",
-      "name": "",
-      "type": "address"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-},
-{
-  "inputs": [],
-  "name": "nftID",
-  "outputs": [
-    {
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-},
-{
-  "inputs": [],
-  "name": "owner",
-  "outputs": [
-    {
-      "internalType": "address",
-      "name": "",
-      "type": "address"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-},
-{
-  "inputs": [],
-  "name": "ownerOfNft",
-  "outputs": [
-    {
-      "internalType": "address",
-      "name": "",
-      "type": "address"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-},
-{
-  "inputs": [],
-  "name": "securityID",
-  "outputs": [
-    {
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-},
-{
-  "inputs": [],
-  "name": "verifiedAddress",
-  "outputs": [
-    {
-      "internalType": "address",
-      "name": "",
-      "type": "address"
-    }
-  ],
-  "stateMutability": "view",
-  "type": "function"
-}
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_nftAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_nftID",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "redeemer",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "enum PerkManager.Perk",
+				"name": "perk",
+				"type": "uint8"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "date",
+				"type": "uint256"
+			}
+		],
+		"name": "NewRedemption",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "enum PerkManager.Perk",
+				"name": "_perk",
+				"type": "uint8"
+			}
+		],
+		"name": "redeemPerk",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "ownerOfNFT",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "newSecurityID",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "date",
+				"type": "uint256"
+			}
+		],
+		"name": "securityIDchanged",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_securityID",
+				"type": "uint256"
+			}
+		],
+		"name": "setSecurityID",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "verifyAddress",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "yearlyUpdate",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "enum PerkManager.Perk",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"name": "amountPerk",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "deadline",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_addressToCheck",
+				"type": "address"
+			}
+		],
+		"name": "getNftBalanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "isVerified",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "nftAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "nftID",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "securityID",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "verifiedAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ]
 
 window.addEventListener("load", async () => {
@@ -280,7 +286,7 @@ window.addEventListener("load", async () => {
         const web3 = new Web3(window.ethereum);
 	await ethereum.request({
    	method: 'wallet_switchEthereumChain',
-   	params: [{ chainId: '0x1' }],
+   	params: [{ chainId: '0x4' }],
   	});
 	await ethereum.request({ method: 'eth_requestAccounts'});
 	const displayAddress = document.getElementById('mm-connect');
@@ -291,11 +297,9 @@ window.addEventListener("load", async () => {
 
 	const rewardProgramContract = new web3.eth.Contract(rewardProgramABI, rewardProgramAddress);
         rewardProgramContract.setProvider(window.ethereum);
-        var nftOwner = await rewardProgramContract.methods.ownerOfNft().call();
-	var nftOwnerLowerCase = nftOwner.toLowerCase();
-	var activeAddressLowerCase = activeAddress.toLowerCase();
-	      
-    	if (nftOwnerLowerCase === activeAddressLowerCase) {
+        var nftOwnerBalance = await rewardProgramContract.methods.getNftBalanceOf(activeAddress).call();
+
+    	if (nftOwnerBalance > 0) {
     	  const changeButton = document.getElementById('myButton1');
 	  changeButton.innerHTML = "Enter";
 	  changeButton.href = "founder-page.html";
@@ -318,7 +322,7 @@ mmEnable.onclick = async () => {
         const web3 = new Web3(window.ethereum);
 	await ethereum.request({
    	method: 'wallet_switchEthereumChain',
-   	params: [{ chainId: '0x1' }],
+   	params: [{ chainId: '0x4' }],
   	});
         await ethereum.request({ method: 'eth_requestAccounts'});
 	const displayAddress = document.getElementById('mm-connect');
@@ -329,11 +333,9 @@ mmEnable.onclick = async () => {
 
 	const rewardProgramContract = new web3.eth.Contract(rewardProgramABI, rewardProgramAddress);
         rewardProgramContract.setProvider(window.ethereum);
-        var nftOwner = await rewardProgramContract.methods.ownerOfNft().call();
-	var nftOwnerLowerCase = nftOwner.toLowerCase();
-	var activeAddressLowerCase = activeAddress.toLowerCase();
-	      
-    	if (nftOwnerLowerCase === activeAddressLowerCase) {
+        var nftOwnerBalance = await rewardProgramContract.methods.getNftBalanceOf(activeAddress).call();
+
+    	if (nftOwnerBalance > 0) {
     	  const changeButton = document.getElementById('myButton1');
 	  changeButton.innerHTML = "Enter";
 	  changeButton.href = "founder-page.html";
